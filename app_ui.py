110168f21ec130d251120751a397ee461df4fd3e
MadMax3366 <102298730+MadMax3366@users.noreply.github.com>
2025-04-29 20:04:14 +0800

    Change db mode

--- app_ui.py
+++ app_ui.py
@@ -8,6 +8,6 @@ app = Playground(agents=[test_agent]).get_app()
 if __name__ == "__main__":
     # Load the knowledge base: Comment after first run as the knowledge base is already loaded
     #test_agent.knowledge.load(upsert=True)
-    test_agent.knowledge.load(recreate=True)
+    test_agent.knowledge.load(skip_existing=True)
 
     serve_playground_app("app_ui:app", reload=True)
